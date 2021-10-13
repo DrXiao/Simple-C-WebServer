ef934b7acbebb36b52d3cda147a6afaad0e48f60
Dr.Xiao <zxc71230@gmail.com>
2021-10-13 15:39:50 +0800

    Modify structure and function parameters
    1. Modify the member of structures: http_request_msg_t and http_response_msg_t.
    
    2. Add enumeration: http_request_method.
    
    3. Add an instance of array named mime_type of mime_type_t.
    
    4. Modify the parameters and implementation of functions: http_parse_*.

--- src/http.c
+++ src/http.c
@@ -10,56 +10,13 @@
 #include <fcntl.h>
 #include "http.h"
 #include "util.h"
-#define MAXLEN 2048
+#define MAXLEN 8192 + 4
 #define MAX_FILE_NAME_LEN 64
+#define HTTP_VER "HTTP/1.1" /* HTTP Start Line */
+
 #if DEBUG == 1
 #include <pthread.h>
 #endif
-
-/* HTTP Start Line */
-#define HTTP_VER "HTTP/1.1"
-
-/* HTTP General Headers
- *
- * Cache-Control
- * Connection
- * Date
- * Pragma
- * Trailer
- * Transfer-Encoding
- * Upgrade
- * Via
- * Warning
- * */
-
-/* HTTP Entity Headers
- *
- * Allow
- * Content-Encoding
- * Content-Language
- * Content-Length
- * Content-Location
- * Content-MD5
- * Content-Range
- * Content-Type
- * Expires
- * Last_Modified
- * extension-header=message-header
- * */
-
-/* HTTP Response headers
- *
- * Accept-Ranges
- * Age
- * ETag
- * Location
- * Proxy-Authenticate
- *
- * Retry-After
- * Server
- * Vary
- * WWW-Authenticate
- * */
 
 enum http_status {
     HTTP_CONTINUE = 100,
@@ -77,8 +34,15 @@ enum http_status {
     HTTP_BAD_GATEWAY = 502
 };
 
-struct http_status_table {
-    uint16_t status;
+enum http_request_method {
+    HTTP_UNKNOWN = 0x0,
+    HTTP_GET = 0x1,
+    HTTP_POST = 0x2,
+    HTTP_HEAD = 0x4
+};
+
+struct http_status_table_t {
+    uint16_t status_code;
     const char *status_name;
 };
 
@@ -88,16 +52,16 @@ struct http_request_msg_t {
     char *request_line, *header, *body;
     // Request line
     int method;
-    char filename[MAX_FILE_NAME_LEN];
     const char *http_ver;
     // Request header
 };
 
 struct http_response_msg_t {
     int client_sockfd;
-    int status;
-    char *filename;
+    struct http_status_table_t *status;
+    char filename[MAX_FILE_NAME_LEN];
     struct stat file_stat;
+    struct mime_type_t *mime;
     char header[MAXLEN];
     char body[MAXLEN];
 };
@@ -109,25 +73,27 @@ struct mime_type_t {
 
 static char *file_dir = "www";
 
-struct http_status_table http_status_table[] = {
+static struct http_status_table_t http_status_table[] = {
     {HTTP_OK, "OK"},
     {HTTP_NOT_MODIFIED, "Not Modified"},
     {HTTP_NOT_FOUND, "Not Found"},
 };
 
-static struct mime_type_t mime[] = {{".html", "text/html"},
-                                    {NULL, "text/plain"}};
-
-static char *request_methods[] = {"GET", "POST", NULL};
+static struct mime_type_t mime_type[] = {{".html", "text/html"},
+                                         {".json", "application/json"},
+                                         {NULL, "text/plain"}};
 
 static void http_parse(struct http_request_msg_t *,
                        struct http_response_msg_t *);
 
-static void http_parse_request_line(struct http_request_msg_t *);
+static void http_parse_request_line(struct http_request_msg_t *,
+                                    struct http_response_msg_t *);
 
-static void http_parse_request_header(struct http_request_msg_t *);
+static void http_parse_request_header(struct http_request_msg_t *,
+                                      struct http_response_msg_t *);
 
-static void http_parse_request_body(struct http_request_msg_t *);
+static void http_parse_request_body(struct http_request_msg_t *,
+                                    struct http_response_msg_t *);
 
 static void http_reply(struct http_response_msg_t *);
 
@@ -154,10 +120,9 @@ void http_request(void *sockfd) {
          sizeof(http_req_msg.recv_msg), 0);
 
     if (!strcmp(http_req_msg.recv_msg, "")) {
-        printf("sockfd(%d) No any msg\n", http_req_msg.client_sockfd);
+        printf("sockfd(%d) No any message\n", http_req_msg.client_sockfd);
         goto close;
     }
-
     http_parse(&http_req_msg, &http_res_msg);
     http_reply(&http_res_msg);
 
@@ -177,77 +142,124 @@ static void http_parse(struct http_request_msg_t *http_req_msg,
 
     parser = strstr(http_req_msg->recv_msg, "\r\n\r\n");
     http_req_msg->body = parser + 4;
-
-    // TODO: Parse start line, header and body
-    http_parse_request_line(http_req_msg);
-    http_res_msg->filename = http_req_msg->filename;
+    // TODO: Parse request line, header and body
+    http_parse_request_line(http_req_msg, http_res_msg);
+    http_parse_request_header(http_req_msg, http_res_msg);
+    http_parse_request_body(http_req_msg, http_res_msg);
 }
 
-static void http_parse_request_line(struct http_request_msg_t *http_req_msg) {
-    char *method, *uri, *http_ver;
+static void http_parse_request_line(struct http_request_msg_t *http_req_msg,
+                                    struct http_response_msg_t *http_res_msg) {
+    char *method = NULL, *uri = NULL, *http_ver = NULL, *parser = NULL;
 
-    http_req_msg->request_line = strtok(http_req_msg->request_line, "\r\n");
+    parser = http_req_msg->request_line;
+    method = parser;
 
-    method = strtok(http_req_msg->request_line, " ");
+    while (*parser != '\r' && *parser != '\n') {
+        if (*parser == ' ') {
+            if (!uri)
+                uri = parser + 1;
+            else if (!http_ver) {
+                http_ver = parser + 1;
+            }
+        }
+        parser++;
+    }
 
-    uri = strtok(NULL, " ");
+    if (!strncmp(method, "GET", 3))
+        http_req_msg->method = HTTP_GET;
+    else if (!strncmp(method, "POST", 4))
+        http_req_msg->method = HTTP_POST;
+    else if (!strncmp(method, "HEAD", 4))
+        http_req_msg->method = HTTP_HEAD;
 
-    http_ver = strtok(NULL, " ");
+    strcat(http_res_msg->filename, file_dir);
+    strncat(http_res_msg->filename, uri, (size_t)(http_ver - uri) - 1);
 
-    snprintf(http_req_msg->filename, MAX_FILE_NAME_LEN, "%s%s", file_dir, uri);
+    if (!strncmp(uri, "/", (size_t)(http_ver - uri) - 1))
+        strcat(http_res_msg->filename, "homepage.html");
 
-    if (!strcmp(uri, "/"))
-        strcat(http_req_msg->filename, "homepage.html");
+    int mime_idx = 0;
+    for (; mime_idx < sizeof(mime_type) / sizeof(struct mime_type_t) - 1;
+         mime_idx++) {
+        if (strstr(http_res_msg->filename, mime_type[mime_idx].type)) {
+            break;
+        }
+    }
+    http_res_msg->mime = mime_type + mime_idx;
+
+    if (!strncmp(http_ver, HTTP_VER, sizeof(HTTP_VER)))
+        http_req_msg->http_ver = HTTP_VER;
 }
 
-static void http_parse_request_header(struct http_request_msg_t *http_req_msg) {
-    return;
+static void
+http_parse_request_header(struct http_request_msg_t *http_req_msg,
+                          struct http_response_msg_t *http_res_msg) {
+    // printf("%s", http_req_msg->header);
 }
 
-static void http_parse_request_body(struct http_request_msg_t *http_req_msg) {
-    return;
+static void http_parse_request_body(struct http_request_msg_t *http_req_msg,
+                                    struct http_response_msg_t *http_res_msg) {
+    if (http_req_msg->method == HTTP_POST) {
+        printf("%s\n", http_req_msg->body);
+    }
 }
 
 static void http_reply(struct http_response_msg_t *http_res_msg) {
 
     if (stat(http_res_msg->filename, &http_res_msg->file_stat) < 0) {
         printf("File: (%s) Not Found\n", http_res_msg->filename);
+        http_res_msg->status = http_status_table + 2;
+        http_res_msg->mime = mime_type;
         http_reply_err(http_res_msg);
-        return;
+        goto reply_end;
     }
 
-    sprintf(http_res_msg->header,
-            "%s 200 OK\r\n"
-            "Server: %s\r\n"
-            "Content-type: text/html\r\n"
-            "Connection: close\r\n"
-            "Content-length: %ld\r\n\r\n",
-            HTTP_VER, "CServer", http_res_msg->file_stat.st_size);
+    http_res_msg->status = http_status_table;
+
+    snprintf(http_res_msg->header, sizeof(http_res_msg->header),
+             "%s %d %s\r\n"
+             "Server: %s\r\n"
+             "Content-type: %s\r\n"
+             "Connection: close\r\n"
+             "Content-length: %ld\r\n\r\n",
+             HTTP_VER, http_res_msg->status->status_code,
+             http_res_msg->status->status_name, "CServer",
+             http_res_msg->mime->value, http_res_msg->file_stat.st_size);
+
+    send(http_res_msg->client_sockfd, http_res_msg->header,
+         strlen(http_res_msg->header), MSG_NOSIGNAL);
+
+    if (http_res_msg->status->status_code == HTTP_HEAD)
+        goto reply_end;
 
     int file_fd = open(http_res_msg->filename, O_RDONLY, 0);
     char *file_addr = mmap(NULL, http_res_msg->file_stat.st_size, PROT_READ,
                            MAP_PRIVATE, file_fd, 0);
     close(file_fd);
-    send(http_res_msg->client_sockfd, http_res_msg->header,
-         strlen(http_res_msg->header), MSG_NOSIGNAL);
     send(http_res_msg->client_sockfd, file_addr,
          http_res_msg->file_stat.st_size, MSG_NOSIGNAL);
     munmap(file_addr, http_res_msg->file_stat.st_size);
+reply_end:
+    return;
 }
 
 static void http_reply_err(struct http_response_msg_t *http_res_msg) {
 
-    sprintf(http_res_msg->body, "<html>"
-                                "<head><title>Not Found</title></head>"
-                                "<body><h1>[404] Not Found</h1></body>"
-                                "</html>");
-    sprintf(http_res_msg->header,
-            "%s 404 Not Found\r\n"
-            "Server: %s\r\n"
-            "Content-type: text/html\r\n"
-            "Connection: close\r\n"
-            "Content-length: %ld\r\n\r\n",
-            HTTP_VER, "CServer", strlen(http_res_msg->body));
+    snprintf(http_res_msg->body, sizeof(http_res_msg->body),
+             "<html>"
+             "<head><title>Not Found</title></head>"
+             "<body><h1>[404] Not Found</h1></body>"
+             "</html>");
+    snprintf(http_res_msg->header, sizeof(http_res_msg->header),
+             "%s %d %s\r\n"
+             "Server: %s\r\n"
+             "Content-type: %s\r\n"
+             "Connection: close\r\n"
+             "Content-length: %ld\r\n\r\n",
+             HTTP_VER, http_res_msg->status->status_code,
+             http_res_msg->status->status_name, "CServer",
+             http_res_msg->mime->value, strlen(http_res_msg->body));
     send(http_res_msg->client_sockfd, http_res_msg->header,
          strlen(http_res_msg->header), MSG_NOSIGNAL);
     send(http_res_msg->client_sockfd, http_res_msg->body,
